b6b39e7d401447343ff60fafda0bea09da8e6287
rgrant01 <zrgran01@gmail.com>
2021-02-25 21:18:57 -0600

    app_trader.sql

--- scripts/app_trader.sql
+++ scripts/app_trader.sql
@@ -1,10 +1,30 @@
+-- Overall comparison between shared apps -- 
+
 SELECT DISTINCT(a.name), p.name, a.price, p.price, a.primary_genre,
 p.genres, a.content_rating, p.content_rating, a.rating, p.rating,
-a.review_count, p.review_count
+a.review_count, p.review_count, p.install_count
 FROM app_store_apps AS a 
 FULL OUTER JOIN play_store_apps AS p
 ON a.name = p.name
-WHERE a.name IS NOT null
-AND p.name IS NOT null
-AND CAST(a.review_count AS int) > 10000
-AND p.review_count > 10000;
+WHERE a.name IS NOT null AND p.name IS NOT null;
+
+
+WITH app AS
+(SELECT
+	name,avg(rating) as app_avg_rating,round(SUM(cast(review_count as int)),2) as app_total_reviews,round(avg(price),2) as app_avg_price
+FROM app_store_apps
+ GROUP BY name
+), play as
+(SELECT
+	name,genres,content_rating,avg(rating) as play_avg_rating,round(SUM(cast(review_count as int)),2) as play_total_reviews,avg(CAST(REPLACE(price,'$','')AS float)) as play_avg_price
+FROM play_store_apps
+ GROUP BY name,genres,content_rating
+)
+SELECT a.name,p.genres,p.content_rating,round(a.app_avg_rating,2) as app_avg_rating ,round(p.play_avg_rating,2) as play_avg_rating,trunc(a.app_total_reviews) as app_all_reviews,trunc(p.play_total_reviews) as play_all_reviews,TRUNC((app_total_reviews+play_total_reviews)/2) as all_total_reviews,a.app_avg_price,p.play_avg_price,
+	CASE WHEN app_avg_price <= 1.00 THEN 10000
+		WHEN app_avg_price >1.00 THEN app_avg_price*10000 END AS App_trader_purchase_price
+FROM app as a
+INNER JOIN play as p
+ON a.name = p.name
+ORDER BY app_avg_rating DESC,play_avg_rating DESC,all_total_reviews DESC,app_avg_price DESC;
+
