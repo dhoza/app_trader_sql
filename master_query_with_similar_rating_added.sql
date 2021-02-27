6d2d8ce847217d0438f3657259666182afa6e5aa
megan2 <megan.amaral@gmail.com>
2021-02-27 13:13:56 -0600

    updated the main query to consider content rating and category/genre after other analysis from team

--- master_query_with_similar_rating_added.sql
+++ master_query_with_similar_rating_added.sql
@@ -1,3 +1,5 @@
+/*This query identifies app with high ratings across both stores, including the content rating, category/genre, and cost to purchase the marketing rights
+*/ 
 WITH app AS
 (SELECT
 	name,avg(rating) as app_avg_rating,round(SUM(cast(review_count as int)),2) as app_total_reviews,round(avg(price),2) as app_avg_price
@@ -5,9 +7,9 @@ FROM app_store_apps
  GROUP BY name
 ), play as
 (SELECT
-	name,genres,content_rating,avg(rating) as play_avg_rating,round(SUM(cast(review_count as int)),2) as play_total_reviews,avg(CAST(REPLACE(price,'$','')AS float)) as play_avg_price
+	name, category, genres,content_rating,avg(rating) as play_avg_rating,round(SUM(cast(review_count as int)),2) as play_total_reviews,avg(CAST(REPLACE(price,'$','')AS float)) as play_avg_price
 FROM play_store_apps
- GROUP BY name,genres,content_rating
+ GROUP BY name,category, genres,content_rating
 ), review_diff AS 
 	(SELECT DISTINCT a.name, a.rating AS app_store_rating, p.rating AS play_store_rating, CAST(sub.difference as numeric)
 	FROM (SELECT a.name AS subname, CAST(a.rating as numeric) - CAST(p.rating as numeric) AS difference
@@ -23,7 +25,7 @@ FROM play_store_apps
 		AND p.rating >= 4
 		AND sub.difference BETWEEN -.2 AND .2)
 		
-SELECT DISTINCT(a.name),r.difference,p.genres,p.content_rating,round(a.app_avg_rating,2) as app_avg_rating ,round(p.play_avg_rating,2) as play_avg_rating,
+SELECT DISTINCT(a.name),r.difference,p.genres,p.category,p.content_rating,round(a.app_avg_rating,2) as app_avg_rating ,round(p.play_avg_rating,2) as play_avg_rating,
 trunc(a.app_total_reviews) as app_all_reviews,trunc(p.play_total_reviews) as play_all_reviews,TRUNC((app_total_reviews+play_total_reviews)/2) as all_total_reviews,
 a.app_avg_price,p.play_avg_price,
 	CASE WHEN app_avg_price <= 1.00 THEN 10000
@@ -33,5 +35,6 @@ INNER JOIN play as p
 ON a.name = p.name
 INNER JOIN review_diff AS r
 ON a.name=r.name
+WHERE p.content_rating='Everyone'
 ORDER BY app_avg_rating DESC,play_avg_rating DESC,all_total_reviews DESC,app_avg_price DESC
-LIMIT 40;
+LIMIT 100;
