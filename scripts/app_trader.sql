950781e3b85d5649e9ec7a8867e0b45738f699cb
rgrant01 <zrgran01@gmail.com>
2021-02-27 13:48:35 -0600

    app_trader.sql

--- scripts/app_trader.sql
+++ scripts/app_trader.sql
@@ -1,4 +1,5 @@
 -- General data from both tables -- 
+
 SELECT *
 FROM app_store_apps;
 
@@ -45,7 +46,7 @@ ELSE 1500*(12*(1+2*rating)) - 10000 * price
 	END),2) as avg_expected_profit
 FROM app_store_apps
 GROUP BY primary_genre
-ORDER BY COUNT(primary_genre) DESC;
+ORDER BY avg_expected_profit DESC;
 
 -- play store -- 
 SELECT category, COUNT(category), ROUND(AVG(
@@ -57,7 +58,7 @@ WHEN CAST(TRIM(REPLACE(price, '$', '')) AS numeric) >= 1
 FROM play_store_apps
 WHERE rating IS NOT NULL
 GROUP BY category
-ORDER BY COUNT(category) DESC;
+ORDER BY avg_expected_profit DESC;
 /* Alternatively order by count of genres */
 
 -- Most expensive apps -- 
@@ -76,11 +77,53 @@ ORDER BY price DESC;
 
 -----------------------------------------------------------------------------------------------------------------------------------------
 
-SELECT *
-FROM play_store_apps
-WHERE category = 'GAME';
+-- Expected profit by content rating -- same as query for category/genre, just plug content rating -- 
+
+-- app store -- 
+SELECT content_rating, COUNT(content_rating), ROUND(AVG(
+CASE WHEN price < 1 
+	THEN 1500*(12*(1+2*rating)) - 10000 
+ELSE 1500*(12*(1+2*rating)) - 10000 * price 
+	END),2) as avg_expected_profit
+FROM app_store_apps
+GROUP BY content_rating
+ORDER BY COUNT(content_rating) DESC;
+
+
+-- play store -- 
+
+SELECT  content_rating, COUNT(content_rating), ROUND(AVG(
+CASE WHEN CAST(TRIM(REPLACE(price, '$', '')) AS numeric) < 1 
+	THEN 1500*(12*(1+2*CAST(rating AS numeric))) - 10000
+WHEN CAST(TRIM(REPLACE(price, '$', '')) AS numeric) >= 1 
+	THEN 1500*(12*(1+2*CAST(rating AS numeric))) - 10000 * CAST(TRIM(REPLACE(price, '$', '')) AS numeric) 
+	END),2) as avg_expected_profit
+FROM play_store_apps AS p
+WHERE rating IS NOT NULL
+GROUP BY content_rating
+ORDER BY avg_expected_profit DESC;
 
 
 
-
+SELECT DISTINCT(a.name), p.name, a.price, p.price, a.primary_genre,
+p.genres, a.content_rating, p.content_rating, a.rating, p.rating,
+a.review_count, p.review_count, p.install_count, ROUND(AVG(
+CASE WHEN a.price < 1 
+	THEN 1500*(12*(1+2*a.rating)) - 10000 
+ELSE 1500*(12*(1+2*a.rating)) - 10000 * a.price 
+	END),2) as avg_expected_profit_app,
+ROUND(AVG(
+CASE WHEN CAST(TRIM(REPLACE(p.price, '$', '')) AS numeric) < 1 
+	THEN 1500*(12*(1+2*CAST(p.rating AS numeric))) - 10000
+WHEN CAST(TRIM(REPLACE(p.price, '$', '')) AS numeric) >= 1 
+	THEN 1500*(12*(1+2*CAST(p.rating AS numeric))) - 10000 * CAST(TRIM(REPLACE(p.price, '$', '')) AS numeric) 
+	END),2) as avg_expected_profit_play
+	FROM app_store_apps AS a 
+FULL OUTER JOIN play_store_apps AS p
+ON a.name = p.name
+WHERE a.name IS NOT null AND p.name IS NOT null
+GROUP BY a.name, p.name, a.price, p.price, a.primary_genre,
+p.genres, a.content_rating, p.content_rating, a.rating, p.rating,
+a.review_count, p.review_count, p.install_count
+ORDER BY avg_expected_profit_play DESC;
 
