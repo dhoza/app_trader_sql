860df0d2d3f2d31ae30ab6c00d7fa5cf30d89cda
dhoza <dhoza@rocketmail.com>
2021-03-01 17:31:07 -0600

    redid ratings and longevity

--- data/app_trader_dhoza.sql
+++ data/app_trader_dhoza.sql
@@ -35,6 +35,7 @@ ORDER BY app_avg_rating DESC,play_avg_rating DESC,all_total_reviews DESC,app_avg
 ---------------------------------------------------------------------------------------------------------------	 
 -----Megan's query integrated with mne__________________________________________
    
+   
 WITH app AS
 (SELECT
 	name,avg(rating) as app_avg_rating,round(SUM(cast(review_count as int)),2) as app_total_reviews,round(avg(price),2) as app_avg_price
@@ -71,7 +72,7 @@ ON a.name = p.name
 INNER JOIN review_diff AS r
 ON a.name=r.name
 ORDER BY app_avg_rating DESC,play_avg_rating DESC,all_total_reviews DESC,app_avg_price DESC
-LIMIT 40;
+LIMIT 100;
 --*******CTE-ultimately getting rid of duplicates by avg 2 tables separately
 --app store -getting app_store_apps stats, average of duplicates CTE !!328 rows
 WITH app AS
@@ -129,7 +130,7 @@ INNER JOIN play as p
 ON a.name = p.name
 ORDER BY app_avg_rating DESC,play_avg_rating DESC,all_total_reviews DESC,app_avg_price DESC;
 
-----GETTING NUMBERS ON TOP 10
+---****-GETTING NUMBERS ON TOP 10***
 WITH app AS
 (SELECT
 	name,avg(rating) as app_avg_rating,round(SUM(cast(review_count as int)),2) as app_total_reviews,round(avg(price),2) as app_avg_price
@@ -142,18 +143,25 @@ FROM play_store_apps
  GROUP BY name,genres,content_rating
 )
 SELECT a.name,
-p.genres,p.content_rating,
-round(a.app_avg_rating,2) as app_avg_rating ,round(p.play_avg_rating,2) as play_avg_rating,trunc(a.app_total_reviews) as app_all_reviews,trunc(p.play_total_reviews) as play_all_reviews,TRUNC(app_total_reviews+play_total_reviews) as all_total_reviews,a.app_avg_price,p.play_avg_price,
+round(CAST((a.app_avg_rating+p.play_avg_rating)AS float))/2 AS avg_rating_both,
+--p.genres,p.content_rating,
+--round(a.app_avg_rating,2) as app_avg_rating ,round(p.play_avg_rating,2) as play_avg_rating,
+--trunc(a.app_total_reviews) as app_all_reviews,trunc(p.play_total_reviews) as play_all_reviews,
+TRUNC(app_total_reviews+play_total_reviews) as all_total_reviews,
+--a.app_avg_price,p.play_avg_price,
 	CASE WHEN app_avg_price <= 1.00 THEN 10000
 		WHEN app_avg_price >1.00 THEN app_avg_price*10000 END AS App_trader_purchase_price,
-		(round(p.play_avg_rating,2)/0.5) as year_longevity,
-		(round(p.play_avg_rating,2)/0.5)*48000-(CASE WHEN app_avg_price <= 1.00 THEN 10000
-		WHEN app_avg_price >1.00 THEN app_avg_price*10000 END) as year_profit
+	((round(CAST((a.app_avg_rating+p.play_avg_rating)AS float))/2)/0.5)+1 as year_longevity,
+	(((round(CAST((a.app_avg_rating+p.play_avg_rating)AS float))/2)/0.5)+1)*48000-(CASE WHEN app_avg_price <= 1.00 THEN 10000
+		WHEN app_avg_price >1.00 THEN app_avg_price*10000 END) AS end_of_longevity_profit
+		
+	--TRUNC((round(p.play_avg_rating,2)/0.5)*48000-(CASE WHEN app_avg_price <= 1.00 THEN 10000
+		--WHEN app_avg_price >1.00 THEN app_avg_price*10000 END)) as End_of_longevity_profit
 FROM app as a
 INNER JOIN play as p 
 ON a.name = p.name
---where A.NAME ilike 'PewDiePie%' OR a.name ILIKE 'Domino%' or a.name ilike 'Egg%' or a.name ilike 'Cytus' or a.name ilike 'ASOS'
-ORDER BY app_avg_rating DESC,play_avg_rating DESC,all_total_reviews DESC,app_avg_price DESC;
+ORDER BY avg_rating_both DESC,all_total_reviews DESC,app_avg_price DESC;
+
 
 
 -----pulling only profit numbers for slide
